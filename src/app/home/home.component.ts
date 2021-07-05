0936ed140dd125e30d16d61a76dad9bb3231acd5
getafemean <getafemean@gmail.com>
2021-07-05 17:11:58 +0200

    end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { SocketServiceService } from '../services/socket-service.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +13,34 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
     form: FormGroup;
+    messagesChat: Subject<any>;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router,
+                private socketService: SocketServiceService) { 
+                        this.messagesChat = <Subject<any>>socketService.connect()
+                                                            .pipe(
+                                                                map((res: any) => {
+                                                                    return res
+                                                                })
+                                                            )
+                 }
 
     ngOnInit(): void {
         this.form = new FormGroup({
             name: new FormControl('')
         })
+        this.messagesChat.subscribe(data => {
+            console.log(data);
+        })
     }
 
     startChat() {
         sessionStorage.setItem('userName', this.form.get('name').value);
+        const data = {
+            label: 'start',
+            name: this.form.get('name').value
+        }
+        this.messagesChat.next(data);
         this.router.navigate(['/chat'])
     }
 
--- src/app/services/socket-service.service.ts
+++ src/app/services/socket-service.service.ts
@@ -18,12 +18,22 @@ export class SocketServiceService {
 
       let observer = {
           next: (data: any) => {
-             this.socket.emit('messageChat', JSON.stringify(data))
+              if(data.label === 'start') {
+                  this.socket.emit('start', JSON.stringify(data));
+              } else {
+                  this.socket.emit('messageChat', JSON.stringify(data));
+              }
           }
       }
 
       let observable = new Observable(observer => {
+          this.socket.on('start', (data: any) => {
+              observer.next(data);
+          })
           this.socket.on('messageChat', (data: any) => {
+              observer.next(data);
+          })
+          this.socket.on('end', (data: any) => {
               observer.next(data);
           })
           return () => { this.socket.disconnect(); }
