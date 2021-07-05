6c50326fd963ff17cecbe240f5d8da11c840aed7
getafemean <getafemean@gmail.com>
2021-07-05 18:34:15 +0200

    home styles and avatar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { SocketServiceService } from '../services/socket-service.service';
@@ -12,8 +12,20 @@ import { map } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
+    avatars: Array<string> = [
+        '/assets/avatar-1.svg',
+        '/assets/avatar-2.svg',
+        '/assets/avatar-3.svg',
+        '/assets/avatar-4.svg',
+        '/assets/avatar-5.svg',
+        '/assets/avatar-6.svg'
+    ]
+
+    @ViewChildren('avatar') avatarsRef: QueryList<ElementRef>;
     form: FormGroup;
     messagesChat: Subject<any>;
+    avatar: string;
+
 
     constructor(private router: Router,
                 private socketService: SocketServiceService) { 
@@ -27,10 +39,22 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         this.form = new FormGroup({
-            name: new FormControl('')
+            name: new FormControl('', [Validators.required, Validators.minLength(2)])
         })
         this.messagesChat.subscribe(data => {
             console.log(data);
+        })
+    }
+
+    setAvatar(i) {
+        this.avatarsRef.forEach((elem: any, index: number) => {
+            if (i === index) {
+                elem.nativeElement.classList.add('selected');
+                this.avatar = this.avatars[i];
+                sessionStorage.setItem('avatar', this.avatar);
+            } else {
+                elem.nativeElement.classList.remove('selected');
+            }
         })
     }
 
